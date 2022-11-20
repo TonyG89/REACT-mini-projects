43e16deb60293db301c9af256476b7fc64532473
Anton Hryhoriev <a.gryhoryev@gmail.com>
2022-11-20 19:20:27 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,49 +1,90 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.scss';
+import Collection from './Collection';
 
-function Collection({ name, images }) {
-  return (
-    <div className="collection">
-      <img className="collection__big" src={images[0]} alt="Item" />
-      <div className="collection__bottom">
-        <img className="collection__mini" src={images[1]} alt="Item" />
-        <img className="collection__mini" src={images[2]} alt="Item" />
-        <img className="collection__mini" src={images[3]} alt="Item" />
-      </div>
-      <h4>{name}</h4>
-    </div>
-  );
-}
 
 function App() {
+  const [search, setSearch] = useState("")
+  const [tagsId, setTagsId] = useState(0)
+  const [isLoading, setIsLoading] = useState(true)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [collection, setCollection] = useState([])
+  const [itemsOnPage, setItemsOnPage] = useState(1)
+
+  const amountPages = Math.ceil(collection.length / itemsOnPage);
+  const tags = ["Все", "Море", "Горы", "Архитектура", "Города"]
+
+  const onSearchInput = obj => obj.name.toLowerCase().includes(search.toLowerCase())
+
+  useEffect(() => {
+    setIsLoading(true)
+
+    const currentTag = obj => obj.category === tagsId
+
+    fetch("./data.json")
+      .then(res => res.json())
+      .then(data => {
+        if (tagsId === 0) {
+          setCollection(data.collections)
+        } else setCollection(data.collections.filter(currentTag))
+      })
+      .catch(err => alert("error - " + err))
+      .finally(() => setIsLoading(false))
+  }, [tagsId, search])
+
   return (
     <div className="App">
       <h1>Моя коллекция фотографий</h1>
       <div className="top">
         <ul className="tags">
-          <li className="active">Все</li>
-          <li>Горы</li>
-          <li>Море</li>
-          <li>Архитектура</li>
-          <li>Города</li>
+          {tags.map((obj, index) => <li
+            key={index}
+            className={tagsId === index && "active"}
+            onClick={() => {
+              setTagsId(index)
+              setCurrentPage(1)
+              console.log(collection)
+            }}
+          >{obj}</li>)}
         </ul>
-        <input className="search-input" placeholder="Поиск по названию" />
+        <input
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+          type="text"
+          className="search-input"
+          placeholder="Поиск по названию" />
+        <ul className="pagination">
+          <li className='active'>Количество коллекций на странице</li>
+          {
+            collection.filter(onSearchInput).map((_, ind) => <li
+              key={ind}
+              onClick={() => setItemsOnPage(ind + 1)}
+              className={itemsOnPage === (ind + 1) && "active"}
+            >{ind + 1}</li>)
+          }
+        </ul>
       </div>
       <div className="content">
-        <Collection
-          name="Путешествие по миру"
-          images={[
-            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          ]}
-        />
+        {isLoading ? (<h2>Идет загрузка...</h2>) : (collection
+          .filter(onSearchInput)
+          .map((obj, index) => (
+            <Collection
+              key={index}
+              name={obj.name}
+              images={obj.photos}
+            />))).slice(currentPage * itemsOnPage - itemsOnPage, currentPage * itemsOnPage)}
       </div>
       <ul className="pagination">
-        <li>1</li>
-        <li className="active">2</li>
-        <li>3</li>
+        {
+          // collection.filter(onSearchInput)
+          [...Array(amountPages)]
+            .map((_, i) => (
+              <li
+              key={i}
+                className={currentPage === (i + 1) && "active"}
+                onClick={() => setCurrentPage(i + 1)}
+              >{i + 1}</li>
+            ))}
       </ul>
     </div>
   );
